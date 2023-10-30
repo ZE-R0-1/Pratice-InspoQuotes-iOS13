084f42f2960fdf9c960eb164ef8dd85883b97933
zero <ZE-R0-1@github.com>
2023-10-30 14:57:40 +0900

    import StoreKit add SKPaymentQueue

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import StoreKit
 
-class QuoteTableViewController: UITableViewController {
+class QuoteTableViewController: UITableViewController, SKPaymentTransactionObserver {
+    
+    
+    let productID = "com.londonappbrewery.InspoQuotesJJ.PremiumQuotes"
     
     var quotesToShow = [
         "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
@@ -31,6 +35,7 @@ class QuoteTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        SKPaymentQueue.default().add(self)
     }
 
     // MARK: - Table view data source
@@ -67,7 +72,26 @@ class QuoteTableViewController: UITableViewController {
     // MARK: - In-App Purchase Msthods
     
     func buyPremiumQuotes() {
-        
+        if SKPaymentQueue.canMakePayments() {
+            let paymentRequest = SKMutablePayment()
+            paymentRequest.productIdentifier = productID
+            SKPaymentQueue.default().add(paymentRequest)
+            
+        } else {
+            print("User can't make payments")
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            if transaction.transactionState == .purchased {
+                //User pasyment successful
+                print("Transaction successful!")
+            } else if transaction.transactionState == .failed {
+                //Payment failed
+                print("Transaction failed!")
+            }
+        }
     }
     
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {
